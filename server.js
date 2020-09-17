aceaa86524690a34b862bc932e81e6e10a8fb902
Manatea <vagner.manatea@gmail.com>
2020-09-17 17:25:47 +0200

    last maj2

--- server.js
+++ server.js
@@ -2,26 +2,79 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidV4 } = require('uuid')
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set(
+  'view engine',
+  'ejs'
+)
+app.use(
+  express.static(
+    'public'
+  )
+)
+app.get(
+  '/',
+  (
+    req,
+    res
+  ) => {
+    res.redirect(`/${uuidV4()}`)
+  }
+)
+app.get(
+  '/:room',
+  (
+    req,
+    res
+  ) => {
+    res.render(
+      'room',
+      {
+        roomId: req.params.room
+      }
+    )
+  }
+)
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidv4()}`)
-})
+io.on(
+  'connection',
+  (
+    socket
+  ) => {
+    socket.on(
+      'join-room',
+      (
+        roomId,
+        userId
+      ) => {
+        socket.join(
+          roomId
+        )
 
-app.get('/:room', (req, res) => {
-  res.render('room', {roomId: req.params.room})
-})
+        socket.to(
+          roomId
+        ).broadcast.emit(
+          'user-connected',
+          userId
+        )
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+        socket.on(
+          'disconnect',
+          (
+
+          ) => {
+            socket.to(
+              roomId
+            ).broadcast.emit(
+              'user-disconnected',
+              userId
+            )
+          }
+        )
+      }
+    )
+  }
+)
+
 server.listen(process.env.PORT ||3000)
