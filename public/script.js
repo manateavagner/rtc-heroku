2950ff9de0f5778edc1800b393d91f5db7235545
Manatea <vagner.manatea@gmail.com>
2020-09-17 16:56:58 +0200

    maj 2

--- public/script.js
+++ public/script.js
@@ -5,16 +5,19 @@ const peers = {}
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
+navigator.mediaDevices.getUserMedia(
+  {
+    video: true,
+    audio: true
+  }).then(stream => {
   addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
     call.answer(stream)
-  })
-
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
   })
